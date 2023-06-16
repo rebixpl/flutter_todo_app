c8bf30cc125c933dd34cbd7d2f2b27610b3ac594
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2023-06-16 15:05:52 +0200

    set up gorouter + theme + home screen page

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_app/presentation/router.dart';
 
 class TodosApp extends StatelessWidget {
   const TodosApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    const color = Colors.green;
+
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'Todos',
+      routerDelegate: router.routerDelegate,
+      routeInformationProvider: router.routeInformationProvider,
+      routeInformationParser: router.routeInformationParser,
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: color,
+          brightness: Brightness.light,
+        ),
+      ),
+      darkTheme: ThemeData.dark(useMaterial3: true).copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: color,
+          brightness: Brightness.dark,
+        ),
+      ),
+      themeMode: ThemeMode.system,
+    );
   }
 }
--- lib/presentation/router.dart
+++ lib/presentation/router.dart
@@ -1,5 +1,12 @@
+import 'package:flutter_todo_app/presentation/view/home.dart';
 import 'package:go_router/go_router.dart';
 
 final router = GoRouter(
-  routes: [],
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const Home(),
+    ),
+  ],
 );
